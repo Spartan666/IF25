0a10934591417a14d7e47cf0d3437804be5d71f2
thomas-dubar <contact.thomas.dubar@gmail.com>
2014-05-16 11:08:15 +0200

    3

--- SimYou/src/modele/Youtube.java
+++ SimYou/src/modele/Youtube.java
@@ -1,34 +1,29 @@
 package modele;
 
+import java.util.ArrayList;
+
 import madkit.gui.menu.LaunchAgentsMenu;
 import madkit.kernel.Agent;
 import madkit.kernel.Madkit;
 import madkit.kernel.Madkit.Option;
 
-public class Youtube extends Agent {
+public class Youtube {
 
 	/**
 	 * @param args
 	 */
-	
+	ArrayList<PosteurVideo> PosteursVideo;
 	
 	public Youtube(){
-		AjouterPosteurVideo();
 	}
 	
-	public void AjouterPosteurVideo(){
-		new PosteurVideo();
-	}
-	
-	public void live() {
-		launchAgent(PosteurVideo.class.getName());
-	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	//	new Madkit(Option.launchAgents.toString(),PosteurVideo.class.getName()+",true,3;");
-		executeThisAgent(1,false,args);
-
+		new PosteurVideo().activate();
+		new PosteurVideo().live();
+		
 	}
 
 }
